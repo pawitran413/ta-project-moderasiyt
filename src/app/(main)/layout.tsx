ad48d87814215051342f125f59442f52d5ba5d01
Amin <aminbagus88@gmail.com>
2026-06-23 18:49:15 +0700

    feat: tambah tombol sign out

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="flex flex-col gap-5 m-10">
+		<div className="flex flex-col gap-5 m-8">
 			<Navbar />
 			{children}
 		</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ProfileIcon from "./icons/ProfileIcon";
+import { useState } from "react";
+import { signOut } from "next-auth/react";
 
 const NAV_LINKS = [
 	{ href: "/dashboard", label: "Dashboard" },
@@ -10,9 +13,10 @@ const NAV_LINKS = [
 
 const Navbar = () => {
 	const pathname = usePathname();
+	const [isProfileOpen, setIsProfileOpen] = useState(false);
 
 	return (
-		<div className="flex w-full justify-center bg-black">
+		<div className="flex w-full justify-center items-center bg-black">
 			<div className="flex border border-white/20 rounded-full p-1">
 				{NAV_LINKS.map(({ href, label }) => (
 					<Link
@@ -24,6 +28,25 @@ const Navbar = () => {
 					</Link>
 				))}
 			</div>
+			<button
+				className="cursor-pointer bg-amber-300"
+				onClick={() => setIsProfileOpen((prev) => !prev)}
+			>
+				<ProfileIcon />
+			</button>
+			{isProfileOpen && (
+				<button
+					onClick={(e) => {
+						if (e.target !== e.currentTarget) {
+							setIsProfileOpen((prev) => !prev);
+						}
+						signOut();
+					}}
+					className="absolute top-22 right-10 px-4 py-2 border border-white/20 text-sm text-red-400 hover:bg-red-500/10 rounded-lg cursor-pointer"
+				>
+					Sign Out
+				</button>
+			)}
 		</div>
 	);
 };
