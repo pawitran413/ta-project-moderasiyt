521759ee9b0a81225510a28e17629bb355c95f98
Amin <aminbagus88@gmail.com>
2026-07-02 15:17:48 +0700

    style: perbaiki posisi icon profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 				))}
 			</div>
 			<button
-				className="cursor-pointer"
+				className="flex items-center cursor-pointer"
 				onClick={() => setIsProfileOpen((prev) => !prev)}
 			>
 				<ProfileIcon />
